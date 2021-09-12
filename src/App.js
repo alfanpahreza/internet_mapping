6eb9d6707b3a01013143246c90dfd0948dd87184
alfanpahreza <54460781+alfanpahreza@users.noreply.github.com>
2021-09-13 06:20:15 +0700

    First

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import source from './jawa-timur.geojson';
+ 
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWxmYW5wayIsImEiOiJja3Q3dzk3d3cwd2Z4MnBvN2VoNjl5dHloIn0.9Io9o4cQ6EZKiphh8Kjybw';
+export default function App() {
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(112.8099); //x axis
+    const [lat, setLat] = useState(-7.7599); //y axis
+    const [zoom, setZoom] = useState(7); //zoom
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [lng, lat],
+            zoom: zoom
+        }); 
+    });
+
+    useEffect(() => {
+        if (!map.current) return;
+        map.current.on('load', () => { //when map loads
+            map.current.addSource(
+                'jatim',{
+                'type': 'geojson',
+                'data': source,
+                }
+            )
+            map.current.addLayer({
+                'id': 'jatim',
+                'type': 'fill',
+                'source': 'jatim', // reference the data source
+                'layout': {},
+                'paint': {
+                    'fill-color': '#58c8ed',
+                    'fill-opacity': 0.3,
+                },
+                'filter': ['==', '$type', 'Polygon'] //filter the types
+            });
+            map.current.addLayer({
+                'id': 'outline',
+                'type': 'line',
+                'source': 'jatim',
+                'layout': {},
+                'paint': {
+                    'line-color': '#000',
+                    'line-width': 0.5
+                },
+                'filter': ['==', '$type', 'Polygon']
+            });
+        })
+    })
+    useEffect(() => {
+        if (!map.current) return; // wait for map to initialize
+        map.current.on('move', () => {
+            setLng(map.current.getCenter().lng.toFixed(4));
+            setLat(map.current.getCenter().lat.toFixed(4));
+            setZoom(map.current.getZoom().toFixed(2));
+        });
+    });
+    return (
+        <div>
+            <div className="sidebar">
+            Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+            </div>
+            <div ref={mapContainer} className="map-container" />
+        </div>
+    );
 }
-
-export default App;
