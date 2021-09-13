e9dba178150e93b2630e1514377fd7324b5496e1
alfanpahreza <54460781+alfanpahreza@users.noreply.github.com>
2021-09-13 08:28:14 +0700

    + Ant Design

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { useRef, useEffect, useState } from 'react';
+import { Layout, Menu } from 'antd';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 import source from './jawa-timur.geojson';
  
 mapboxgl.accessToken = 'pk.eyJ1IjoiYWxmYW5wayIsImEiOiJja3Q3dzk3d3cwd2Z4MnBvN2VoNjl5dHloIn0.9Io9o4cQ6EZKiphh8Kjybw';
-export default function App() {
+function App() {
     const mapContainer = useRef(null);
     const map = useRef(null);
     const [lng, setLng] = useState(112.8099); //x axis
     const [lat, setLat] = useState(-7.7599); //y axis
     const [zoom, setZoom] = useState(7); //zoom
+
+    const { Header, Content, Footer } = Layout;
 
     useEffect(() => {
         if (map.current) return; // initialize map only once
@@ -30,7 +33,7 @@ export default function App() {
                 }
             )
             map.current.addLayer({
-                'id': 'jatim',
+                'id': 'polygon',
                 'type': 'fill',
                 'source': 'jatim', // reference the data source
                 'layout': {},
@@ -44,7 +47,10 @@ export default function App() {
                 'id': 'outline',
                 'type': 'line',
                 'source': 'jatim',
-                'layout': {},
+                'layout': { 
+                    'line-cap': 'round',
+                    'line-join': 'round',
+                },
                 'paint': {
                     'line-color': '#000',
                     'line-width': 0.5
@@ -61,12 +67,35 @@ export default function App() {
             setZoom(map.current.getZoom().toFixed(2));
         });
     });
+
     return (
-        <div>
-            <div className="sidebar">
-            Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-            </div>
-            <div ref={mapContainer} className="map-container" />
+        <div className="App">
+            <Layout className="layout">
+                <Header>
+                    <div className="logo" />
+                    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+                    <Menu.Item key="1">
+                        Map
+                    </Menu.Item>
+                    <Menu.Item key="2">
+                        Info
+                    </Menu.Item>
+                    </Menu>
+                </Header>
+                <Content style={{ padding: '0 50px' }}>
+                    <div className="site-layout-content">
+                        <div className="sidebar">
+                            Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+                        </div>
+                        <div ref={mapContainer} className="map-container" />
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>
+                    Ant Design ©2018 Created by Ant UED
+                </Footer>
+            </Layout>
         </div>
     );
 }
+
+export default App;
